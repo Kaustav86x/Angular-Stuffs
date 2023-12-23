1d4b2c6389699262a23c7536681e9184d6fe2ea4
Kaustav <kaustav86x@gmail.com>
2023-12-24 02:58:44 +0530

    24-12-23
    Making components and building logics

--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 
+// a decorator is a ts feature used to enhance the elemenets we use in our code...
 @Component({
     selector : 'app-server',
     templateUrl: './server.component.html'
---
+++ my-first-angular-app/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+  h1 
+  {
+    color : 	#ff8000; 
+  }
+  `]
+})
+export class WarningAlertComponent {
+
+}
