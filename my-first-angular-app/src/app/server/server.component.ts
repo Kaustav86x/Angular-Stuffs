dbbbc46ac014e328ea2aced4cbeec43b3371c216
Kaustav <kaustav86x@gmail.com>
2024-01-01 23:57:04 +0530

    01-01-24
    Applying CSS Classes dynamically

--- my-first-angular-app/src/app/server/server.component.ts
+++ my-first-angular-app/src/app/server/server.component.ts
@@ -3,21 +3,41 @@ import { Component, OnInit } from "@angular/core";
 // a decorator is a ts feature used to enhance the elemenets we use in our code...
 @Component({
     selector : 'app-server', // element
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online {
+        color : white;
+    }`]
 })
 
-export class ServerComponent implements  OnInit {
+// export class ServerComponent implements  OnInit {
 
-    // isUserNameEmpty = false;
-    username = "";
-    // output = "Nothing to see here !!";
+//     // isUserNameEmpty = false;
+//     username = "";
+//     // output = "Nothing to see here !!";
 
-    ngOnInit() {
+//     ngOnInit() {
         
+//     }
+//     OnGivivngUserNameInput()
+//     {
+//         this.username;
+//         this.username = "";
+//     }
+// }
+export class ServerComponent 
+{
+    serverId : number = 10;
+    serverStatus : string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
-    OnGivivngUserNameInput()
+    getServerStatus()
     {
-        this.username;
-        this.username = "";
+        this.serverStatus;
+    }
+    getColor() {
+        return this.serverStatus ===  'online' ? 'green' : 'red';
     }
 }
--- my-first-angular-app/src/app/servers/servers.component.ts
+++ my-first-angular-app/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',  // attribute
+  // selector: '[app-servers]',  // attribute
+  selector: 'app-servers',
   // selector: 'app-servers',      // elememt
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
